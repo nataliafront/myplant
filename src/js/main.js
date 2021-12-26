e155ea29288a747ae449dab747214b58d84416ae
nataliafront <nataliapanina0219@gmail.com>
2021-12-26 18:17:20 +0200

    add pages

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const select = () => {
 	});
 	selectItem.forEach((item) => {
 		item.addEventListener('click', (e) => {
-			e.currentTarget.closest('.select').querySelector('.select__current').innerHTML = e.currentTarget.innerHTML;
+			e.currentTarget.closest('.select').querySelector('.select__current').innerHTML = e.currentTarget.innerHTML.toLowerCase();
 			e.currentTarget.closest('.select').classList.remove('select--opened');
 		})
 	});
